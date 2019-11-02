8017153480370ac59b9357c7ad4fe6e5efd95c40
wbswjc <me@wbswjc.com>
2019-11-03 01:19:08 +0800

    coverage

--- scripts/test.py
+++ scripts/test.py
@@ -8,4 +8,4 @@ root = os.path.abspath(os.path.join(__file__, '../..'))
 venv_dir = 'venv'
 venv_path = os.path.join(root, venv_dir)
 
-Popen([os.path.join(venv_path, 'bin', 'pytest')]).wait()
+Popen([os.path.join(venv_path, 'bin', 'pytest'), '--cov', 'skeleton']).wait()
