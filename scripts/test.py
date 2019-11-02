3a79bdc2b973aa985436672ab279c2df711c17d3
wbswjc <me@wbswjc.com>
2019-11-03 01:39:46 +0800

    coverage

--- scripts/test.py
+++ scripts/test.py
@@ -8,4 +8,5 @@ root = os.path.abspath(os.path.join(__file__, '../..'))
 venv_path = os.path.join(root, 'venv')
 
 Popen([os.path.join(venv_path, 'bin', 'pytest'),
-       '--cov', os.path.join(root, 'skeleton')]).wait()
+       '--cov', os.path.join(root, 'skeleton'), '--cov-report', 'term-missing',
+       os.path.join(root, 'tests')]).wait()
